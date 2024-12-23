718d7e2913df8fdac62c46a35d3dc05928bdf6cb
Leonardo <leonardo3442@hotmail.com>
2024-12-23 19:57:46 -0300

    test: check-in use case

--- src/use-cases/check-in.ts
+++ src/use-cases/check-in.ts
@@ -1,10 +1,14 @@
 import { ICheckInsRepository } from "@/repositories/check-ins-repository";
+import { GymsRepository } from "@/repositories/gyms-repository";
 import { CheckIn } from "@prisma/client";
+import { ResourceNotFoundError } from "./errors/resource-not-found-error";
 
 
 interface CheckInUseCaseRequest {
     userId: string;
     gymId: string;
+    userLatitude: number
+    userLongitude: number
 }
 
 interface CheckInUseCaseResponse {
@@ -12,14 +16,27 @@ interface CheckInUseCaseResponse {
 }
 
 export class CheckInUseCase {
-    constructor(checkInsRepository: ICheckInsRepository) {
+    private checkInsRepository: ICheckInsRepository;
+    private gymsRepository: GymsRepository;
+
+    constructor(checkInsRepository: ICheckInsRepository, gymsRepository: GymsRepository) {
         this.checkInsRepository = checkInsRepository;
+        this.gymsRepository = gymsRepository;
     }
 
-    private checkInsRepository: ICheckInsRepository;
-
     async execute({ userId, gymId }: CheckInUseCaseRequest): Promise<CheckInUseCaseResponse> {
-        const checkIn = await this.checkInsRepository.create({ gym_id: gymId, user_id: userId });
+
+        const gym = await this.gymsRepository.findById(gymId)
+        if (!gym) {
+            throw new ResourceNotFoundError()
+        }
+
+        const checkIn = await this.checkInsRepository.create({
+            gym_id: gymId,
+            user_id: userId
+        });
+
+
         return { checkIn };
     }
 
